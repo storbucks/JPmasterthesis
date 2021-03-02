c82e4a8907c7e9b975811b88e8ace50106e4797d
storbucks <jpng@tutanota.de>
2021-03-02 17:41:48 +0100

    mist

--- scrape_comments.py
+++ scrape_comments.py
@@ -1,7 +1,7 @@
 import praw
 import pandas as pd
 
-# gain acces through api
+# API access
 reddit = praw.Reddit(
     user_agent="comment_scrape",
     client_id="xCoeA1iqMfQEsA",
@@ -9,8 +9,14 @@ reddit = praw.Reddit(
     username="bakingandfinance",
     # password="hf)ua7(:ski9@1",
 )
+# # list of subreddits to examine to automate it or write code for each sr to keep structure understandable and organized?
+# srs = ["finance", "wallstreetbets"]
+# # access subreddit
+# for i in srs:
+#     sr = reddit.subreddit(i)
+#     print(sr)
 
-# access subreddit
+
 sr = reddit.subreddit("finance")
 
 titles = []
@@ -38,7 +44,10 @@ df = pd.DataFrame(
 # make dictionary for every submisson
 comm_dict = {i:[] for i in ids}
 # store list of comments to each submission in dictionary
-count = 0
-for comment in sub.comments.list():
-    comm_dict[ids[count]].append(comment.body)
-    count += 1
+
+for sub in sr.hot(limit=5):
+    count = 0
+
+    for comment in sub.comments.list():
+        while count < len(ids):
+            comm_dict[ids[count]].append(comment.body)count += 1
