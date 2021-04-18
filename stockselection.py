db32594244eca8b3288a048b8192942d95897c68
storbucks <76474717+storbucks@users.noreply.github.com>
2021-04-18 15:09:51 +0200

    working on extracting text wrt selected stocktags

--- stockselection.py
+++ stockselection.py
@@ -6,27 +6,37 @@ api = PushshiftAPI()
 start_epoch = int(datetime.datetime(2021, 1, 1).timestamp())  # START DATE FROM WHERE OFF WE SCRAPE
 end_epoch = int(datetime.datetime(2021, 3, 31).timestamp())
 
-# submissions = list(api.search_submissions(after=start_epoch, subreddit='wallstreetbets',
-#                                           filter=['url','author', 'title', 'subreddit'],
-#                                           limit=100))
-#%% FILTER SUBMISSIONS THAT CONTAIN CASHTAG IN TITLE
-# for submission in submissions:
-#     words = submission.title.split()
-#     # print(words)
-#     cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
-#     if len(cashtags) > 0:
-#         print(cashtags)
-#         print(submission.title)
+submissions = list(api.search_submissions(after=start_epoch, subreddit='wallstreetbets',
+                                          filter=['url','author', 'title', 'subreddit']
+                                          ))
+# FILTER SUBMISSIONS THAT CONTAIN CASHTAG IN TITLE
+for submission in submissions:
+    words = submission.title.split()
+    # print(words)
+    cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
+    if len(cashtags) > 0:
+        print(cashtags)
+        print(submission.title)
 
-#%% SEARCH COMMENTS FOR STOCK TAG XY
+#%% SEARCH COMMENTS
+# per day
+# per subreddit
+# per tag
+
+subr = ['wallstreetbets', 'finance', 'pizza']
 stocktags = ['$GME', '$BTC', '$AMC']
 
 # make dictionary for every stocktag
+sr_dict = {sr:[] for sr in subr}
 comment_dict = {tag:[] for tag in stocktags}
+
+# for sr in range(len(subr)):
+#     subr_new = subr[sr]
+#     new_dic = {}
 
 for tag in stocktags:
     print(tag)
-    wsb_search = api.search_comments(q=tag, subreddit='wallstreetbets', after=start_epoch, before=end_epoch)
+    wsb_search = api.search_comments(q=tag, subreddit=subr[0], after=start_epoch, before=end_epoch)
 
     max_response_cache = 10  # for testing, to be removed for real analysis
     cache = []  # for testing, to be removed for real analysis
@@ -39,6 +49,8 @@ for tag in stocktags:
             break
 
 
+
+
 # #STORE EACH COMMENT AS LIST OBJECT IN A LIST
 # wsb_comments = []
 # i = 0
@@ -46,3 +58,12 @@ for tag in stocktags:
 #     print(cache[i].body)
 #     wsb_comments.append(cache[i].body)
 #     i += 1
+
+#%%
+# import pandas as pd
+# subr = ['wallstreetbets', 'finance', 'pizza']
+# stocktags = ['$GME', '$BTC', '$AMC']
+#
+# frame = {'Day':[1,2,3], 'SR':subr, 'Tag':stocktags, 'comments':[3,2,1]}
+#
+# df = pd.DataFrame(frame)
