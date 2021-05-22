8a9300bc174380d490879c555d34cb2ff60d63de
storbucks <76474717+storbucks@users.noreply.github.com>
2021-05-22 12:45:54 +0200

    first convnet keras test with reddit embeddings model accuracy 17%, val 11% (quite low) still work 2do

---
+++ keras_test.py
@@ -0,0 +1,187 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+## Download the Newsgroup20 data
+data_path = keras.utils.get_file(
+    "news20.tar.gz",
+    "http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.tar.gz",
+    untar=True,
+)
+#%%
+import os
+import pathlib
+## Look at the data
+data_dir = pathlib.Path(data_path).parent / "20_newsgroup"
+dirnames = os.listdir(data_dir)
+print("Number of directories:", len(dirnames))
+print("Directory names:", dirnames)
+
+fnames = os.listdir(data_dir / "comp.graphics")
+print("Number of files in comp.graphics:", len(fnames))
+print("Some example filenames:", fnames[:5])
+
+#%%
+## Here's a example of what one file contains:
+print(open(data_dir / "comp.graphics" / "38987").read())
+
+#%%
+"""
+As you can see, there are header lines that are leaking the file's category, either
+explicitly (the first line is literally the category name), or implicitly, e.g. via the
+`Organization` filed. Let's get rid of the headers:
+"""
+
+samples = []
+labels = []
+class_names = []
+class_index = 0
+for dirname in sorted(os.listdir(data_dir)):
+    class_names.append(dirname)
+    dirpath = data_dir / dirname
+    fnames = os.listdir(dirpath)
+    print("Processing %s, %d files found" % (dirname, len(fnames)))
+    for fname in fnames:
+        fpath = dirpath / fname
+        f = open(fpath, encoding="latin-1")
+        content = f.read()
+        lines = content.split("\n")
+        lines = lines[10:]
+        content = "\n".join(lines)
+        samples.append(content)
+        labels.append(class_index)
+    class_index += 1
+
+print("Classes:", class_names)
+print("Number of samples:", len(samples))
+
+#%%
+## Shuffle and split the data into training & validation sets
+# Shuffle the data
+seed = 1337
+rng = np.random.RandomState(seed)
+rng.shuffle(samples)
+rng = np.random.RandomState(seed)
+rng.shuffle(labels)
+
+# Extract a training & validation split
+validation_split = 0.2
+num_validation_samples = int(validation_split * len(samples))
+train_samples = samples[:-num_validation_samples]
+val_samples = samples[-num_validation_samples:]
+train_labels = labels[:-num_validation_samples]
+val_labels = labels[-num_validation_samples:]
+
+#%%
+"""
+## Create a vocabulary index
+Let's use the `TextVectorization` to index the vocabulary found in the dataset.
+Later, we'll use the same layer instance to vectorize the samples.
+Our layer will only consider the top 20,000 words, and will truncate or pad sequences to
+be actually 200 tokens long.
+"""
+
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+vectorizer = TextVectorization(max_tokens=7000, output_sequence_length=200)  # changed to 7000 (< num of words)
+text_ds = tf.data.Dataset.from_tensor_slices(train_samples).batch(128)
+vectorizer.adapt(text_ds)
+
+"""
+You can retrieve the computed vocabulary used via `vectorizer.get_vocabulary()`. Let's
+print the top 5 words:
+"""
+
+print(vectorizer.get_vocabulary()[:5])  #added print
+
+#%%
+"""
+Let's vectorize a test sentence:
+"""
+
+output = vectorizer([["the cat sat on the mat"]])
+print(output.numpy()[0, :6])  #added print
+
+"""
+As you can see, "the" gets represented as "2". Why not 0, given that "the" was the first
+word in the vocabulary? That's because index 0 is reserved for padding and index 1 is
+reserved for "out of vocabulary" tokens.
+Here's a dict mapping words to their indices:
+"""
+
+voc = vectorizer.get_vocabulary()
+word_index = dict(zip(voc, range(len(voc))))
+
+"""
+As you can see, we obtain the same encoding as above for our test sentence:
+"""
+
+test = ["the", "cat", "sat", "on", "the", "mat"]
+print([word_index[w] for w in test])  #added print
+
+#%%
+from gensim.models.word2vec import Word2Vec
+## Load pretrained WE model
+w2v_model = Word2Vec.load("w2v_model_v2.model")
+
+#%%
+## load pre-trained WE matrix into an 'embedding' layer
+num_tokens = len(w2v_model.wv.index_to_key)
+embedding_dim = w2v_model.vector_size
+embedding_matrix = w2v_model.syn1neg
+
+from tensorflow.keras.layers import Embedding
+
+embedding_layer = Embedding(
+    num_tokens,
+    embedding_dim,
+    embeddings_initializer=keras.initializers.Constant(embedding_matrix),
+    trainable=False,
+)
+
+#%%
+"""
+## Build the model
+A simple 1D convnet with global max pooling and a classifier at the end.
+"""
+
+from tensorflow.keras import layers
+
+int_sequences_input = keras.Input(shape=(None,), dtype="int64")
+embedded_sequences = embedding_layer(int_sequences_input)
+x = layers.Conv1D(128, 5, activation="relu")(embedded_sequences)
+x = layers.MaxPooling1D(5)(x)
+x = layers.Conv1D(128, 5, activation="relu")(x)
+x = layers.MaxPooling1D(5)(x)
+x = layers.Conv1D(128, 5, activation="relu")(x)
+x = layers.GlobalMaxPooling1D()(x)
+x = layers.Dense(128, activation="relu")(x)
+x = layers.Dropout(0.5)(x)
+preds = layers.Dense(len(class_names), activation="softmax")(x)
+model = keras.Model(int_sequences_input, preds)
+model.summary()
+
+#%%
+"""
+## Train the model
+First, convert our list-of-strings data to NumPy arrays of integer indices. The arrays
+are right-padded.
+"""
+
+x_train = vectorizer(np.array([[s] for s in train_samples])).numpy()
+x_val = vectorizer(np.array([[s] for s in val_samples])).numpy()
+
+y_train = np.array(train_labels)
+y_val = np.array(val_labels)
+
+"""
+We use categorical crossentropy as our loss since we're doing softmax classification.
+Moreover, we use `sparse_categorical_crossentropy` since our labels are integers.
+"""
+
+model.compile(
+    loss="sparse_categorical_crossentropy", optimizer="rmsprop", metrics=["acc"]
+)
+model.fit(x_train, y_train, batch_size=128, epochs=20, validation_data=(x_val, y_val))
